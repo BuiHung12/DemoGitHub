976608870a79d93f126db55e24f0fe323c758911
hung <buihung0625@gmail.com>
2022-02-06 16:17:39 +0700

    branch-2-2 comit 1

--- DemoProject/src/main/java/com/example/demoproject/controller/TestController.java
+++ DemoProject/src/main/java/com/example/demoproject/controller/TestController.java
@@ -1,7 +1,16 @@
 package com.example.demoproject.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping
 public class TestController {
+
+    @GetMapping
+    public void getMethod(){
+        System.out.println("this is branch ");
+    }
+
 }
